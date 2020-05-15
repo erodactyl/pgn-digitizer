47b9265267a63cc2abeca86b7b0572c7d9cc662d
Erik Davtyan <erikdavtyan@gmail.com>
2020-05-15 22:08:07 +0400

    Fix typescript errors

--- build/getScannedGame.js
+++ build/getScannedGame.js
@@ -14,22 +14,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const scanImage_1 = __importDefault(require("./scanImage"));
 const path_1 = __importDefault(require("path"));
-const splitMoves_1 = __importDefault(require("./splitMoves"));
-const getGameById_1 = __importDefault(require("./getGameById"));
-const moveDifferences_1 = __importDefault(require("./moveDifferences"));
 const getScannedGame = (id, debug) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const scannedPgn = yield scanImage_1.default(path_1.default.join(__dirname, "..", "testImages", `${id}.png`));
         return scannedPgn;
-        const moves = splitMoves_1.default(scannedPgn);
-        const realMoves = splitMoves_1.default(getGameById_1.default(id));
-        if (debug) {
-            console.log("Scanned: ", scannedPgn, "\n");
-            console.log("Real: ", getGameById_1.default(id), "\n");
-            for (let i = 0; i < Math.max(moves.length, realMoves.length); i++) {
-            }
-        }
-        return moveDifferences_1.default(moves, realMoves);
     }
     catch (e) {
         console.error(e);
--- src/getScannedGame.ts
+++ src/getScannedGame.ts
@@ -12,26 +12,6 @@ const getScannedGame = async (id: string, debug?: boolean): Promise<string> => {
     );
 
     return scannedPgn;
-
-    const moves = splitMoves(scannedPgn);
-
-    const realMoves = splitMoves(getGameById(id));
-
-    if (debug) {
-      console.log("Scanned: ", scannedPgn, "\n");
-      console.log("Real: ", getGameById(id), "\n");
-      for (let i = 0; i < Math.max(moves.length, realMoves.length); i++) {
-        // console.log(`${i} scanned is ${moves[i]}, real is ${realMoves[i]}`);
-      }
-    }
-
-    // const pgn = traverseGame(moves);
-
-    // console.log(pgn);
-
-    // const traversedMoves = splitMoves(pgn);
-
-    return moveDifferences(moves, realMoves);
   } catch (e) {
     console.error(e);
   }
